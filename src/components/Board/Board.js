a551879305f26445452cc3d7690bbb7e58afc761
Igor <igor9levi@gmail.com>
2018-08-22 02:51:02 +0200

    initial commit

---
+++ src/components/Board/Board.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+
+export default class Board extends PureComponent {
+    state = {  }
+    render() {
+        return (
+            <div class='board'>Hello</div>
+        );
+    }
+}
