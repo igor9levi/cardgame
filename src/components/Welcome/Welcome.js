1630a33816c81048021450433619693b3ff396ff
Igor <igor9levi@gmail.com>
2018-09-04 22:49:54 +0200

    fix(lint errors): 2 space erro

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Welcome = props => (
+export const Welcome = ({setNumPlayers}) => (
   <div>
     <h1 className="welcome-message">Hello! Please choose number of players</h1>
-    <button>
-        Two Players
+    <button
+      type="button"
+      onClick={() => {
+        setNumPlayers(2);
+      }}
+    >
+      Two Players
     </button>
-    <button>
-        Three Players
+    <button
+      type="button"
+      onClick={() => {
+        setNumPlayers(3);
+      }}
+    >
+      Three Players
     </button>
-    <button>
-        Four Players
+    <button
+      type="button"
+      onClick={() => {
+        setNumPlayers(4);
+      }}
+    >
+      Four Players
     </button>
   </div>
 );
+
+Welcome.propTypes = {
+  setNumPlayers: PropTypes.func.isRequired,
+};
 
 export default Welcome;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import reducer from '../reducers';
 
+const middleware = [logger];
+
 const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION && window._REDUX_DEVTOOLS_EXTENSION__()
+  reducer,
+  applyMiddleware(...middleware),
+  // window.__REDUX_DEVTOOLS_EXTENSION && window._REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 export default store;
