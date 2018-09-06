e0a3ffe346d7083c6ece5c42fc311f369d7fe715
Igor <igor9levi@gmail.com>
2018-09-06 05:48:40 +0200

    feat: play cards to center

--- src/components/Board/Card/Card.js
+++ src/components/Board/Card/Card.js
@@ -1,33 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Card.css';
 
 class Card extends React.Component {
+  static propTypes = {
+    playerId: PropTypes.number.isRequired,
+    center: PropTypes.object.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.cardRef = React.createRef();
+  }
+
   state = {
     cardStatus: 'player-card',
     styling: {},
     height: 50,
   }
 
-  calculateTablePosition = playerId => {
+  calculateTablePosition = ({ playerId, cardHeight }) => {
     switch (playerId) {
       case 0:
         return {
           left: 0,
-          top: 50,
+          top: cardHeight,
         };
       case 1:
         return {
-          left: -50,
+          left: -cardHeight,
           top: 0,
         };
       case 2:
         return {
           left: 0,
-          top: -50,
+          top: -cardHeight,
         };
       case 3:
         return {
-          left: 50,
+          left: cardHeight,
           top: 0,
         };
       default:
@@ -40,7 +51,8 @@ class Card extends React.Component {
 
   animateCard = () => {
     const { center, playerId } = this.props;
-    const { top, left } = this.calculateTablePosition(playerId)
+    const cardHeight = this.cardRef.current.height;
+    const { top, left } = this.calculateTablePosition({ playerId, cardHeight });
 
     this.setState({
       // cardStatus: 'player-card animate',
@@ -80,6 +92,7 @@ class Card extends React.Component {
 
     return (
       <img
+        ref={this.cardRef}
         className={cardStatus}
         alt={alt}
         src={src}
